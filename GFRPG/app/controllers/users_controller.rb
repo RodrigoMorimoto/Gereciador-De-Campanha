cc6a3b6c34567bf378d3262be140d0ae02506da9
Fiipe B. Chaves <fipebc@gmail.com>
2018-12-03 14:12:46 -0200

    alteração da view new user

--- GFRPG/app/controllers/users_controller.rb
+++ GFRPG/app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:User_name, :User_secret)
+	  #params.fetch(:user, {})
     end
 end
